fb3c796bac5737bc997c8d9f07cc89346ba9a04e
daijianlin <daijianlin@163.com>
2017-01-13 00:11:37 +0800

    step 7 upload resume

--- app/models/job.rb
+++ app/models/job.rb
@@ -1,4 +1,6 @@
 class Job < ApplicationRecord
+  has_many :resumes
+  
   validates :title, presence: true
   validates :wage_upper_bound, presence: true
   validates :wage_lower_bound, presence: true
@@ -6,7 +8,7 @@ class Job < ApplicationRecord
 
   scope :published, -> { where(is_hidden: false) }
   scope :recent, -> { order('created_at DESC') }
-  
+
   def publish!
     self.is_hidden = false
     self.save
